a20feb5415f56a0cdfd371876ae364ce93506790
Kuria-Gtonye <stevekuriagatonye@gmail.com>
2025-08-05 16:05:14 +0300

    Grouped all sterile equipment together

---
+++ H_set.py
@@ -0,0 +1,14 @@
+#sterile items
+sterile_lab = {"Microscope","Centrifigure","Pippete"}
+sterile_er = {"Stethoscope","Defribrator","Sutures"}
+
+#contaminated items
+contaminated ={"Defibrator","Sutures","Pippete"}
+
+sterile_er -= contaminated
+print("Sterile er equipment", sterile_er)
+sterile_lab -= contaminated
+print("Sterile lab equipment", sterile_lab)
+
+all_sterile = sterile_lab | sterile_er
+print("All equipment:" ,all_sterile)
