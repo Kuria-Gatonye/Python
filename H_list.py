57b3cd18690f71116fca8e5b9d88960a4227ae6d
Kuria-Gtonye <stevekuriagatonye@gmail.com>
2025-08-05 15:34:41 +0300

    Poped/Removed the item on position 1

--- H_list.py
+++ H_list.py
@@ -1,5 +1,11 @@
 ward_4a = [" Bed1: Kuria","Bed2: Fatuma"]
+
 #new admission
 ward_4a.append("Bed3: Viktor")
 ward_4a.insert(1 , "Bed4: James")
+
 print("Ward4A:", ward_4a)
+
+#Discharge patients
+discharged = ward_4a.pop(1)
+print(f"Discharged: {discharged} remaining: {ward_4a}")
